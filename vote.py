a4d7bb8e80815babde84ecb45ac232459663d8fa
cmplex <cedrichaase@gmail.com>
2013-06-09 20:39:54 +0200

    Change vote system functionality Add automated vote threshold calculation based on number of clients Add volume increasing functionality to 'hype'-vote

--- vote.py
+++ vote.py
@@ -1,22 +1,42 @@
 # vim: tabstop=3 shiftwidth=3 noexpandtab
 # @author Cedric Haase
 
+import math
+import mpd
+
 HOST='localhost'
 PORT='6600'
 
-ABS_NEXT_THRESHOLD = 2
-ABS_HYPE_THRESHOLD = 2
-
-import mpd
+MAX_VOL_PERC = 70
+CLIENTS_CONNECTED = 3
 
 client = mpd.MPDClient();
 
+def getThreshold():
+	# TODO: get number of currently connected clients
+	clients_connected = CLIENTS_CONNECTED
+
+	# scale relative vote threshold according to number of connected clients
+	if(clients_connected > 1):
+		rel_threshold = 1 / math.log(clients_connected, 2)
+	else:
+		rel_threshold = 1
+
+	# compute absolute vote threshold (round to nearest integer)
+	abs_threshold = int(round((clients_connected * rel_threshold),0))
+
+	return abs_threshold
+
+
+
 def checkVotes(path):
+	abs_vote_threshold = getThreshold()
+
 	client.connect(HOST, PORT)
 
 	stickers = client.sticker_list("song", path)
 
-	# count total amount of next votes
+	# count total amount of next/hype votes
 	nextvotes=0
 	hypevotes=0
 	for sticker in stickers:
@@ -26,14 +46,24 @@ def checkVotes(path):
 			hypevotes+=1
 
 	# skip song on skipvote threshold
-	if(nextvotes >= ABS_NEXT_THRESHOLD):
+	if(nextvotes >= abs_vote_threshold):
 		client.sticker_delete("song", path)
 		client.next()
 
-	# enqueue song again on hype threshold
-	if(hypevotes >= ABS_HYPE_THRESHOLD):
-		client.sticker_delete("song", path)
-		client.add(path)	
+	# raise volume according to number of hype votes
+	# TODO get volume cap from database or config file
+	# cap volume at MAX_VOL_PERC
+	if(hypevotes < abs_vote_threshold):
+		hype_volume = (float(hypevotes)/float(abs_vote_threshold)) * MAX_VOL_PERC
+		hype_volume = int(round(hype_volume,0))
+	else:
+		hype_volume = MAX_VOL_PERC
+	print hype_volume
+	cur_volume	= int(client.status()['volume'])
+	if(hype_volume > cur_volume):
+		# TODO remember original value & reset on next track
+		# increase volume
+		client.setvol(hype_volume)
 
 	client.close()
 	client.disconnect()
@@ -48,14 +78,11 @@ def voteNext(clientid):
 	# set 'next' vote for current clientID and song
 	client.sticker_set("song", path, clientid, "next")
 
-	retval = client.sticker_get("song", path, clientid)
-
 	client.close()
 	client.disconnect()
 
 	checkVotes(path)
 
-	return retval
 
 def voteHype(clientid):
 	client.connect(HOST, PORT)
@@ -66,11 +93,23 @@ def voteHype(clientid):
 	# set 'hype' vote for current clientID and song
 	client.sticker_set("song", path, clientid, "hype")
 
-	retval = client.sticker_get("song", path, clientid)
-	
 	client.close()
 	client.disconnect()
 
 	checkVotes(path)
 
-	return retval
+##################################
+#		M O D U L E  T E S T S		#
+##################################
+
+if __name__ == "__main__":
+	MAX_VOL_PERC = 70
+	CLIENTS_CONNECTED = 20
+	voteHype(1)
+	voteHype(2)
+	voteHype(3)
+	voteHype(4)
+	voteHype(5)
+	voteHype(6)
+	voteHype(7)
+	voteHype(8)
