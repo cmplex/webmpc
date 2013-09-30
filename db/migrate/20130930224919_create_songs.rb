83b99fd9703b5b01220911e13b07794469222bc4
Sebastian Neuser <haggl@sineband.de>
2013-10-01 00:50:34 +0200

    Add song model

---
+++ db/migrate/20130930224919_create_songs.rb
@@ -0,0 +1,11 @@
+class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string :artist
+      t.string :album
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130930221108) do
+ActiveRecord::Schema.define(version: 20130930224919) do
+
+  create_table "songs", force: true do |t|
+    t.string   "artist"
+    t.string   "album"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",     null: false
