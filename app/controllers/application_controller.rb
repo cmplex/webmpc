0e9b27d253309d3f8d6639537b626b27e7ee0611
Sebastian Neuser <haggl@sineband.de>
2013-10-02 04:29:35 +0200

    Initialize MPD connection and read song info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,22 @@
+# vim: tabstop=2 shiftwidth=2 expandtab
 class ApplicationController < ActionController::Base
+  after_filter :close_mpd_connection
+  before_filter :open_mpd_connection
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   rescue_from CanCan::AccessDenied do |exception|
-      redirect_to root_url, alert: exception.message
+    redirect_to root_url, alert: exception.message
+  end
+
+  def open_mpd_connection
+    @mpc = MPD.new
+    @mpc.connect
+  end
+
+  def close_mpd_connection
+    @mpc.disconnect
   end
 end
--- app/controllers/now_playing_controller.rb
+++ app/controllers/now_playing_controller.rb
@@ -1,2 +1,6 @@
+# vim: tabstop=2 shiftwidth=2 expandtab
 class NowPlayingController < ApplicationController
+  def index
+    @song = @mpc.current_song
+  end
 end
