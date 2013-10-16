24e4adb60201922cbadf93ea4e17dd6131774fb7
Sebastian Neuser <haggl@sineband.de>
2013-10-16 04:14:20 +0200

    Add vote references to song model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,6 @@
 class Song < ActiveRecord::Base
+  has_many :hype_votes
+  has_many :hypers, through: :hype_votes, source: :user
+  has_many :hate_votes
+  has_many :haters, through: :hate_votes, source: :user
 end
