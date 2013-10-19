df2572e1e27b528f6265bb12c333eba92535bdb9
Sebastian Neuser <haggl@sineband.de>
2013-10-19 15:28:36 +0200

    Use rails link_to method and styled <a> tags for main menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 # vim: tabstop=2 shiftwidth=2 expandtab
 module ApplicationHelper
   def current?(path)
-    "current" if current_page?(path)
+    return "current" if current_page?(path)
+  end
+
+  def nav_link(link_text, path)
+    link_to link_text, path, class: current?(path), onFocus: "blur();"
   end
 end
