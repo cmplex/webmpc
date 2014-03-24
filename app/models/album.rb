ffea8a6ed6a5d6bbf02a82647e49ba82eb28d7ac
Cedric Haase <cedrichaase@gmail.com>
2014-03-24 02:27:06 +0100

    Change album cover default size

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,5 @@
 class Album < ActiveRecord::Base
   has_many :songs
-  has_attached_file :cover, :styles => { :default => "500x500#", :thumb => "80x80#" }, :default_url => "default.png"
+  has_attached_file :cover, :styles => { :default => "300x300#", :thumb => "80x80#" }, :default_url => "default.png"
   validates_attachment :cover, :content_type => { :content_type => ["image/jpg", "image/gif", "image/png", "image/jpeg"] }
 end
